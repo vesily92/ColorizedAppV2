70ead2d30d653cd70e9c7c814d787c8b7512602c
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-10-30 18:35:00 +0300

    Initial Commit

---
+++ ColorizedAppV2/ColorizedAppV2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppV2App.swift
+//  ColorizedAppV2
+//
+//  Created by Василий Пронин on 30.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
