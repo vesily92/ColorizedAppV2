af658be1fdd3288da60eb81b829deb2d83f033a7
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-11-02 16:50:33 +0300

    ColorTunerView setup

---
+++ ColorizedAppV2/ColorTunerView.swift
@@ -0,0 +1,37 @@
+//
+//  ColorTunerView.swift
+//  ColorizedAppV2
+//
+//  Created by Василий Пронин on 02.11.2021.
+//
+
+import SwiftUI
+
+struct ColorTunerView: View {
+    
+    @Binding var sliderValue: Double
+    @State private var textValue = ""
+    
+    let color: Color
+    
+    var body: some View {
+        HStack {
+            ValueTextView(sliderValue: sliderValue)
+            
+            ValueSliderView(sliderValue: $sliderValue, textValue: $textValue, color: color)
+            
+            ValueTextFieldView(sliderValue: $sliderValue, textValue: $textValue)
+        }
+        .onAppear {
+            textValue = "\(lround(sliderValue))"
+        }
+    }
+}
+
+
+
+struct ColorTunerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorTunerView(sliderValue: .constant(90), color: .red)
+    }
+}
